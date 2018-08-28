014c3cbb1b9c35161c80e5c22cffcc485c18902f
Karl Hornlund <karlhornlung@gmail.com>
2018-08-28 18:17:47 +0930

    Updated .rmd with new plot showing expected trials for width=0.05

--- script.R
+++ script.R
@@ -1,107 +1,53 @@
 library(tidyverse)
 library(ggthemes)
 
-#------------------------------------------------------------------
-# Functions
-#------------------------------------------------------------------
-
-# get N samples of binary outcomes with probability p
-get_samples <- function(N, p) {
-  samples <- sample(x = c(1, 0), size = N, replace = TRUE, prob = c(p, 1-p))
-  return(samples)
-}
-
-# given a set of sample lists, subset the first N
-# samples of each list and compute the average
-# confidence interval size
-get_width_for_N <- function(N, samples) {
-  sample_subset <- data.frame(samples[1:N, ])
-  stats <- map_dfr(sample_subset, compute_stats)
-  return(mean(stats$size))
-}
-
-compute_stats <- function(x) {
-  q  <- qnorm(.975) # 95% confidence interval
-  N  <- length(x)
-  s  <- sd(x)
-  mu <- mean(x)
-  
-  lower <- mu - q * s / sqrt(N)
-  upper <- mu + q * s / sqrt(N)
-  size <- upper - lower
-  
-  stats <- list(
-    "mu" = mu,
-    "sd" = s,
-    "N"  = N,
-    "lower" = lower,
-    "upper" = upper,
-    "size" = size
-  )
-  
-  return(stats)
-}
-
-get_widths <- function(p, B, runs) {
-  samples <- replicate(B, get_samples(max(runs), p))
-  sizes <- map(runs, get_width_for_N, samples = samples)
-  names(sizes) <- runs
-  
-  return(sizes)
-}
-
-#------------------------------------------------------------------
-# Execution
-#------------------------------------------------------------------
-
-B <- 1000 # repeats
 probs <- seq(0.50, 0.99, 0.01)
-runs <- seq(100, 700, 100)
+N     <- seq(50, 1000, 1)
+runs  <- seq(200, 800, 100)
 
-results.df <- cbind(probs, map_dfr(probs, get_widths, B = B, runs = runs))
-names(results.df)[1] <- "P"
+width_func <- function(p, N) {
+  std_dev <- sqrt(p * (1-p))
+  z <- qnorm(.975) # 95% CI
+  width <- 2 * z * std_dev/sqrt(N)
+  return(width)
+}
 
-widths.df <- results.df %>%
-  gather(key = N, value = Width, -P) %>%
-  arrange(desc(P))
-  
-intercepts <- widths.df %>%
-  filter(Width < 0.05) %>%
-  group_by(N) %>%
-  summarise(Intercept = min(P))
-#------------------------------------------------------------------
-# Plotting
-#------------------------------------------------------------------
+df <- expand.grid(N, probs)
+names(df) <- c("N", "P")
 
-widths.df %>%
-  ggplot(aes(x = P, y = Width, color = N)) +
+df <- df %>%
+  mutate(width = width_func(.$P, .$N))
+
+df %>%
+  filter(N %in% runs) %>%
+  ggplot(aes(x = P, y = width, color = factor(N))) +
   geom_line() +
   scale_color_gdocs() +
   theme_gdocs() +
   labs(
-    title = "Width of 95% Confidence Interval",
-    x = "Probability of Binary Outcome",
-    y = "Width of 95% Confidence Interval",
-    color = "Runs"
+    title = "Confidence Interval Widths for Bernoulli Trials",
+    x = "Probability of Bernoulli Trial Success (p)",
+    y = "E[ Width of 95% Confidence Interval ]",
+    color = "Trials (N)"
   )
 
-intercepts %>%
-  ggplot(aes(x = as.numeric(N), y = Intercept)) +
-  geom_point() +
+
+min.df <- df %>%
+  filter(width <= 0.05) %>%
+  group_by(P) %>%
+  summarise(min_runs = min(N))
+
+min.df %>%
+  ggplot(aes(x = P, y = min_runs)) +
   geom_line() +
+  scale_color_gdocs() +
   theme_gdocs() +
+  scale_x_continuous(breaks = seq(0.8, 1, 0.02)) +
   labs(
-    title = "Convergence to 0.05 Confidence Interval Width",
-    x = "Runs",
-    y = "Probability at which Confidence Interval width is 0.05"
+    title = "Trials required for 0.05 Confidence Interval Width",
+    x = "Probability of Bernoulli Trial Success (p)",
+    y = "E[ Trials ]"
   )
-
-print.df <- results.df[seq(1, nrow(results.df), by = 5), ]
-print(map_dfr(print.df, round, digits = 3))
-
-
-
-
 
 
 
